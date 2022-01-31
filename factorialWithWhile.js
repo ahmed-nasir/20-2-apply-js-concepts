1fbc7937fca5991c215d142082dafdc55f460b82
Nasir Ahmed <nasirahmed595@gmail.com>
2022-01-31 16:07:28 +0600

    i--

--- factorialWithWhile.js
+++ factorialWithWhile.js
@@ -1,12 +1,25 @@
-function getFactorial(number) {
-    let factorial = 1;
-    let i = 1
-    while (i <= number) {
-        factorial = factorial * i;
-        i++
+// function getFactorial(number) {
+//     let factorial = 1;
+//     let i = 1
+//     while (i <= number) {
+//         factorial = factorial * i;
+//         i++
 
+//     }
+//     return factorial;
+// }
+// console.log(getFactorial(4));
+
+
+
+
+function getFactorial(num) {
+    let factorial = num;
+    let i = 1;
+    while (i >= 1) {
+        factorial = factorial * num;
+        i--;
     }
     return factorial;
 }
 console.log(getFactorial(4));
-
